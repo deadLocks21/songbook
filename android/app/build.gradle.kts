70fdbffc730e04100b3ecce6655265e463590f4d
Tim HOFMANN <timothe@hofmann.fr>
2026-01-07 10:15:53 +0100

    refactor: update application namespace and identifiers to fr.dtfh.songbook

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.songbook"
+    namespace = "fr.dtfh.songbook"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.songbook"
+        applicationId = "fr.dtfh.songbook"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
