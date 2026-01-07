35880612cd1990e5e56dd36464dc73f1698c8d75
Tim HOFMANN <timothe@hofmann.fr>
2026-01-07 10:59:12 +0100

    feat: implement HomePage and SettingsPage with navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'ui/pages/home/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: Scaffold(body: const Center(child: Text('Hello, World!'))),
+      home: const HomePage(),
     );
   }
 }
---
+++ lib/ui/pages/home/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:songbook/ui/pages/settings/settings_page.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Songbook'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.settings),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const SettingsPage()),
+              );
+            },
+          ),
+        ],
+      ),
+      body: const Center(child: Text('Hello, World!')),
+    );
+  }
+}
